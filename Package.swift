0902ad1c99bdf371bbb56e355a3c4ff4cdc14b04
Harsh Shandilya <harsh.shandilya@dyte.io>
2025-08-12 12:56:57 +0530

    chore(release): update RealtimeKitUI to 0.4.2

--- Package.swift
+++ Package.swift
@@ -2,27 +2,29 @@
 import PackageDescription
 
 let package = Package(
-  name: "RealtimeKitUI",
-  platforms: [.iOS(.v13)],
-  products: [
-    .library(name: "RealtimeKitUI", targets: ["RealtimeKitUI"])
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/dyte-in/RealtimeKitCoreiOS.git",
-      revision: "5473910f19a62a96c9b7cdd8d9c08f6829f560ea")
-  ],
-  targets: [
-    .target(
-      name: "RealtimeKitUI",
-      dependencies: [
-        .product(name: "RealtimeKit", package: "RealtimeKitCoreiOS"),
-        .product(name: "RTKWebRTC", package: "RealtimeKitCoreiOS"),
-      ],
-      path: "RealtimeKitUI/",
-      resources: [
-        .process("Resources/notification_join.mp3"),
-        .process("Resources/notification_message.mp3"),
-      ])
-  ]
+    name: "RealtimeKitUI",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "RealtimeKitUI", targets: ["RealtimeKitUI"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/dyte-in/RealtimeKitCoreiOS.git",
+            revision: "8af7acefb58799c1ede8d4e445678be9cdf199d7"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "RealtimeKitUI",
+            dependencies: [
+                .product(name: "RealtimeKit", package: "RealtimeKitCoreiOS"),
+                .product(name: "RTKWebRTC", package: "RealtimeKitCoreiOS"),
+            ],
+            path: "RealtimeKitUI/",
+            resources: [
+                .process("Resources/notification_join.mp3"),
+                .process("Resources/notification_message.mp3"),
+            ]
+        ),
+    ]
 )
